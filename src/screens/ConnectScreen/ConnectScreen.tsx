8931c14e34f2fbb95c9b36d3482f24f8750cfc2e
benk10 <ben.kaufman10@gmail.com>
2024-08-29 12:19:43 +0400

    Add QR regenerate btn

--- src/screens/ConnectScreen/ConnectScreen.tsx
+++ src/screens/ConnectScreen/ConnectScreen.tsx
@@ -1,8 +1,10 @@
 import styles from "./ConnectScreen.module.css";
 import keeperLogo from "../../assets/keeper-logo.png";
 import instructionsIcon from "../../assets/instructions-icon.svg";
+import refreshIcon from "../../assets/refresh.svg";
 
-import { useEffect, useState } from 'react';
+
+import { useEffect, useState, useCallback } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import { listen } from '@tauri-apps/api/event';
 import { useNavigate } from "react-router-dom";
@@ -12,12 +14,13 @@ const ConnectScreen = () => {
   const navigate = useNavigate();
   const [qrData, setQrData] = useState('');
 
-  useEffect(() => {
-    const setupChannel = async () => {    
-      const channelSecret = await invoke<string>('generate_encryption_key');
-      setQrData(channelSecret);
-    };
+  const setupChannel = useCallback(async () => {    
+    const channelSecret = await invoke<string>('generate_encryption_key');
+    setQrData(channelSecret);
+  }, []);
 
+
+  useEffect(() => {
     setupChannel();
 
     // Listen for SET_NETWORK event
@@ -26,14 +29,15 @@ const ConnectScreen = () => {
       if (event === 'SET_NETWORK' && data[0].network) {
         const network = data[0].network === 'TESTNET' ? 'TESTNET' : 'MAINNET';
         console.log(`Network set to: ${network}`);
-        navigate('/home', { replace: true });
+        openModal();
+        // navigate('/home', { replace: true });
       }
     });
 
     return () => {
       unsubscribe.then(f => f());
     };
-  }, []);
+  }, [setupChannel]); // Add setupChannel to the dependency array
 
   return (
     <div className={styles.connectScreen}>
@@ -42,27 +46,44 @@ const ConnectScreen = () => {
         <h1 className={styles.title}>Welcome to Keeper Desktop App</h1>
         <div className={styles.instructionsContainer}>
           <h3 className={styles.instructionsTitle}>
-            <img src={instructionsIcon} className={styles.instructionsIcon} alt="" />
+            <img
+              src={instructionsIcon}
+              className={styles.instructionsIcon}
+              alt=""
+            />
             Instructions:
           </h3>
           <ul className={styles.instructionsList}>
             <li>From the keeper app, open QR scanner and Scan this app</li>
-            <li>You can use this for adding a device, registering a vault, or to sign transactions</li>
-            <li>You will need a new QR for each operation you perform, by just refreshing</li>
+            <li>
+              You can use this for adding a device, registering a vault, or to
+              sign transactions
+            </li>
+            <li>
+              You will need a new QR for each operation you perform, by just
+              refreshing
+            </li>
           </ul>
         </div>
         <div className={styles.note}>
           <h4>Note:</h4>
-          <p>This QR will get you the key to Decrypt the data from this Keeper Web Interface to make communication E2R encrypted</p>
+          <p>
+            This QR will get you the key to Decrypt the data from this Keeper
+            Web Interface to make communication E2R encrypted
+          </p>
         </div>
       </div>
       <div className={styles.qrContainer}>
         <div className={styles.qrCode}>
           <QRCode value={qrData} size={200} bgColor="#2d6759" fgColor="#fff" />
           <p className={styles.scanMe}>Scan Me</p>
+          <button className={styles.regenerateButton} onClick={setupChannel}>
+            <img src={refreshIcon} alt="Regenerate QR Code" />
+          </button>
         </div>
         <p className={styles.qrNote}>
-          Make sure No one is around you when you scan this. You can always refresh to get fresh key session
+          Make sure No one is around you when you scan this. You can always
+          refresh to get fresh key session
         </p>
       </div>
     </div>

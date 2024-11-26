423d4480684ab570add5d0752530a0c3d32388ec
benk10 <ben.kaufman10@gmail.com>
2024-11-26 12:14:25 +0400

    Support taproot and fix duplicated m in path

--- src-tauri/src/device.rs
+++ src-tauri/src/device.rs
@@ -7,14 +7,17 @@ use serde_json::json;
 pub enum ScriptType {
     P2WPKH,
     P2WSH,
+    P2TR,
 }
 
 pub fn get_xpubs(hwi_state: &HWIClientState, account: u32) -> Result<serde_json::Value, HWIError> {
     let ss_path = get_derivation_path(ScriptType::P2WPKH, hwi_state.network, account);
     let ms_path = get_derivation_path(ScriptType::P2WSH, hwi_state.network, account);
+    let tr_path = get_derivation_path(ScriptType::P2TR, hwi_state.network, account);
 
     let single_sig_xpub = hwi_state.hwi.get_xpub(&ss_path, false)?;
     let multi_sig_xpub = hwi_state.hwi.get_xpub(&ms_path, false)?;
+    let taproot_xpub = hwi_state.hwi.get_xpub(&tr_path, false)?;
 
     if hwi_state.fingerprint.is_none() {
         return Err(HWIError::Hwi(
@@ -24,10 +27,12 @@ pub fn get_xpubs(hwi_state: &HWIClientState, account: u32) -> Result<serde_json:
     }
 
     Ok(json!({
-        "singleSigPath": format!("m/{}", ss_path.to_string()),
+        "singleSigPath": ss_path.to_string(),
         "singleSigXpub": single_sig_xpub.to_string(),
-        "multiSigPath": format!("m/{}", ms_path.to_string()),
+        "multiSigPath": ms_path.to_string(),
         "multiSigXpub": multi_sig_xpub.to_string(),
+        "taprootPath": tr_path.to_string(),
+        "taprootXpub": taproot_xpub.to_string(),
         "mfp": hwi_state.fingerprint.as_ref().unwrap().to_string().to_uppercase(),
     }))
 }
@@ -43,6 +48,9 @@ fn get_derivation_path(script_type: ScriptType, network: Network, account: u32) 
             .parse()
             .unwrap(),
         ScriptType::P2WSH => format!("m/48'/{}'/{}'/2'", network_num, account)
+            .parse()
+            .unwrap(),
+        ScriptType::P2TR => format!("m/86'/{}'/{}'", network_num, account)
             .parse()
             .unwrap(),
     }
